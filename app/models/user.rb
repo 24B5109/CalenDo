1b5f938e459a383961b032f806c5a44ccbd6b26a
24B5109 <meixiaobeisen@gmail.com>
2025-11-01 21:20:58 +0900

    a

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          has_many :blogs, dependent: :destroy
-         validates :name, presence: true
          validates :profile, length: { maximum: 200 }
 end
